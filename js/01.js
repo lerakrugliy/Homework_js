548b061b7e05e12dbb7f9c98dbdf78cf3299834e
lerakrugliy <lerakrugliy@gmail.com>
2026-02-07 18:08:59 +0200

    01 ✅

---
+++ js/01.js
@@ -0,0 +1,31 @@
+const age = 13;
+console.log(age);
+
+const name = "Лєра";
+console.log(name);
+
+const message = "Ти cтудент?";
+const isStudent = confirm(message);
+console.log(isStudent);
+
+const myString = "Хорошою людиною може бути той хто палить, а поганою людиною може бути той хто ходить в церкву";
+console.log(myString);
+
+let myNumber = 2;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+const actionDangerous = "Увага! Дія може бути небезпечною.";
+alert(actionDangerous);
+const youSure = "Ви впевнені, що хочете продовжити?";
+confirm(youSure);
